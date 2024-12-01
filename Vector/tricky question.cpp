8cd63f2160be69e0af917b3ecc316a2cd2f81049
Abhay Dixit <dixitshivam534@gmail.com>
2024-12-01 19:56:34 +0530

    folder manage

--- Vector/tricky question.cpp
+++ Vector/tricky question.cpp
@@ -1,5 +1,6 @@
 // if we pass  integer number it will run int between int and float and doouble
-// but if we call function with decimal values it will casuse ambiguity and if we call write double it will not cause ambiguity because doubke have priority 
+// but if we call function with decimal values it will casuse ambiguity and if
+// we call write double it will not cause ambiguity because doubke have priority 
 
 
 #include <iostream>
@@ -36,9 +37,11 @@ double maxs(double  a,double b){
 }
 
 int main(){
-	cout<<maxs(3,4);  // if we  call maxs fuction with 3,4 it will case ambiguity becase int and float has same priority and memroy size  but if we call it without flaot function it
+	cout<<maxs(3,4);  // if we  call maxs fuction with 3,4 it will case ambiguity becase int 
+//	and float has same priority and memroy size  but if we call it without flaot function it
 	//will not cause any error and it will run int funtion beacuse int accept intiger number but double accepts decimal
 	cout<<maxs(3.5,4.9);
-	 // if we  call maxs fuction with 3.5,4.9 it will cause ambiguity becase int and float has same priority and memroy size  but if we call it without flaot function it
+	 // if we  call maxs fuction with 3.5,4.9 it will cause ambiguity becase int and float 
+//	 has same priority and memroy size  but if we call it without flaot function it
 	//will not cause any error and it will run int funtion beacuse int accept intiger number but double accepts decimal
 }
